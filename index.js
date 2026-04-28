a083af0f13074caed2a8aa61c64221bb735add9b
Apple-skiner2631 <appleskiner2631@gmail.com>
2026-04-28 11:37:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 async function sendToWebhook(mcId, isBedrock) {
     const finalId = isBedrock ? `.${mcId}` : mcId;
-    const webhookUrl = '你的WEBHOOK_URL';
+    const webhookUrl = 'https://discord.com/api/webhooks/1497711106779648281/8UfMJSZ0PB_Jfj9du31Gqsj5iNbjmekdHvdyLg2jsHTqcVXViyQKakVDupdZLLRGDTH4';
     
     await axios.post(webhookUrl, {
         content: `!run whitelist add ${finalId}`
